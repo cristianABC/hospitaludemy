9241b17796611f795962adc3bc84112961c406b5
Cristian <cristianarangodaza@gmail.com>
2019-11-11 10:11:18 -0500

    Observable, Breadcrumbs, MetaTags, HTML Title

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  OnDestroy } from '@angular/core';
+import { Observable, Subscriber, Subscription } from 'rxjs';
+import {retry,map,filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs',
   templateUrl: './rxjs.component.html',
   styles: []
 })
-export class RxjsComponent implements OnInit {
+export class RxjsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  subscription: Subscription;
+
+  constructor() { 
+     
+    this.subscription = this.regresaObservable().subscribe(
+      numero => console.log('subscribed', numero),
+      error=> console.error('Error en el obs', error),
+      () => console.log('El observador termino')
+    )
+    // NUMERO(NEXT, ERROR(ERROR),  (Completado) 
+  }
 
   ngOnInit() {
   }
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
+  
+  regresaObservable():Observable<any>{
+   return new Observable((observer:Subscriber<any>) =>{
+      let contador = 0 ;
+      let intervalo = setInterval(()=>{
+        contador +=1;
+        const salida = {
+          valor:contador
+        };
+        observer.next(salida);
 
+        // if(contador === 3){
+        //   clearInterval(intervalo);
+        //   observer.complete();
+
+        // }
+    
+      }, 1000);
+    }).pipe(
+      map(resp =>  resp.valor     ),
+      filter((valor,index)=> {
+        if((valor%2)===1){
+          //impar
+          return true;
+        }
+        else{
+          //par 
+          return false;
+        }
+      })
+    );
+    
+  }
 }
